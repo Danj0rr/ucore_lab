ebd1efd915e3b83e4b6c0625cda3c879284c4515
primus <dlwjc0713@163.com>
2021-06-21 12:56:23 +0800

    debug1

--- lab5/kern/debug/kdebug.c
+++ lab5/kern/debug/kdebug.c
@@ -347,5 +347,17 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+     uint32_t ebp=read_ebp(), eip=read_eip();
+     for(int i=0;i<STACKFRAME_DEPTH && ebp!=0;i++){
+     
+     uint32_t* call_arguments = (uint32_t*)ebp+2;
+     cprintf("ebp:0x%08x eip:0x%08x args:0x%08x 0x%08x 0x%08x 0x%08x",
+             ebp,eip,call_arguments[0],call_arguments[1],
+             call_arguments[2],call_arguments[3]);
+     cprintf("\n");
+     print_debuginfo(eip-1);
+     eip=*(((uint32_t*)ebp)+1);
+     ebp=*((uint32_t*)ebp);
+     } 
 }
 
--- lab5/kern/fs/fs.h
+++ lab5/kern/fs/fs.h
@@ -2,10 +2,10 @@
 #define __KERN_FS_FS_H__
 
 #include <mmu.h>
-
+//一个扇区512b，一个页占八个扇区
 #define SECTSIZE            512
 #define PAGE_NSECT          (PGSIZE / SECTSIZE)
-
+//使用1号硬盘
 #define SWAP_DEV_NO         1
 
 #endif /* !__KERN_FS_FS_H__ */
